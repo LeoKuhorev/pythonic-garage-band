20b1f83c9f78e80789b018cf96c15d5b2fc41bfa
Leo Kukharau <kuhorev.leonid@gmail.com>
2020-06-10 14:35:29 -0700

    iniitial commit

---
+++ pythonic_garage_band/pythonic_garage_band.py
@@ -0,0 +1,15 @@
+class Musician:
+    def __init__(self, name):
+        self.name
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def __repr__(self):
+        return super().__repr__()
+
+    def get_instrument(self):
+        return f''
+
+    def play_solo(self):
+        return f''
---
+++ tests/test_pythonic_garage_band.py
@@ -0,0 +1,8 @@
+from pythonic_garage_band.pythonic_garage_band import Musician
+
+
+class TestMusician:
+    m = Musician()
+
+    def test_str(self):
+        pass
